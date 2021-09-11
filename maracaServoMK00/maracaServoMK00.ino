339cf1dae64df54f13edb1eecca7eca7f0176997
Hari Prasanth <24803379+hariChozhan@users.noreply.github.com>
2021-09-12 04:29:21 +0530

    Add files via upload

---
+++ maracaServoMK00/maracaServoMK00.ino
@@ -0,0 +1,23 @@
+#include <Servo.h>
+Servo maracaServo1;
+Servo maracaServo2;
+
+void setup() {
+  maracaServo1.attach(4);   // Maraca 1 attached to arduino Digital 2 pin
+  maracaServo2.attach(5);   // Maraca 2 attached to arduino Digital 3 pin
+}
+
+void loop() {
+  maracaServo1.write(0);    // Maraca 1 moves to 0 angle
+  maracaServo1.write(90);   // Maraca 1 moves to 90 angle
+  delay(500);             // Time delay in ms ie 1000ms = 1 second
+  maracaServo2.write(0);    // Maraca 2 moves to 0 angle
+  maracaServo2.write(90);   // Maraca 2 moves to 90 angle
+  delay(500);
+  maracaServo1.write(0);
+  maracaServo1.write(90);
+  delay(500);
+  maracaServo2.write(0);
+  maracaServo2.write(90);
+  delay(500);           //Copy and paste the above set of codes and change the servo angles and time delay to make some tunes 
+}
